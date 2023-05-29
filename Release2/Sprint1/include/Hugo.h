d557b9b1922827b2661bbfa44ec2dd094146c66b
FievreMur <hoarauhugo3108@gmail.com>
2023-05-29 11:58:35 +0200

    o

--- Release2/Sprint1/include/Hugo.h
+++ Release2/Sprint1/include/Hugo.h
@@ -1,3 +1,12 @@
 
 
 int remove_dot_segments(char reduct_path[]);
+
+int chek_version(Node *root, int clientId);
+
+int check_connection(Node *root, int clientId);
+
+int check_Transfer_Encoding(Node *root, int clientId);
+
+int remove_dot_segments(char reduct_path[]);
+
