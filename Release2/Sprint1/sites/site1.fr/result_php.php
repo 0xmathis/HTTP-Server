c340b0351d3017a5d1be8773c285965c32f761e3
Nathan <torres201665@gmail.com>
2023-06-01 10:16:12 +0200

    oui

--- Release2/Sprint1/sites/site1.fr/result_php.php
+++ Release2/Sprint1/sites/site1.fr/result_php.php
@@ -22,7 +22,7 @@
         <?php
             if ((!isset($_GET['calcul']) || !isset($_GET['vitesse']) || !isset($_GET['kendo'])) && !isset($_POST['last']))
             {
-                printf(('Il faut répondre à toutes les questions pour soumettr)e le formulaire.');
+                echo "Il faut répondre à toutes les questions pour soumettre le formulaire.";
 
                 // Arrête l'exécution de PHP
                 return;
@@ -34,35 +34,35 @@
                 $score = 0;
                 
                 if($_GET['calcul'] == 0){
-                    printf( "Maths: 1/1    Bien joué\n");
+                    echo "Maths: 1/1    Bien joué\n";
                     $score+=1;
                 }
                 else{
-                    printf( "Le résultat du calcul était 0. Va falloir réviser...\n");
+                    echo "Le résultat du calcul était 0. Va falloir réviser...\n";
                 }
 
                 if($_GET['vitesse'] == 3e8){
-                    printf( "Physique: 1/1    Bien joué\n");
+                    echo "Physique: 1/1    Bien joué\n";
                     $score+=1;
                 }
                 else{
-                    printf( "La lumière se déplace à 3e8 m/s. Va falloir réviser...\n");
+                    echo "La lumière se déplace à 3e8 m/s. Va falloir réviser...\n";
                 }
 
                 if(strcmp($_GET['kendo'],"bambou") == 0){
-                    printf( "Sport: 1/1    Bravo\n");
+                    echo "Sport: 1/1    Bravo\n";
                     $score+=1;
                 }
                 else{
-                    printf( "La réponse était le bambou. Il faut se cultiver un peu. Lisez un peu plus de mangas.\n");
+                    echo "La réponse était le bambou. Il faut se cultiver un peu. Lisez un peu plus de mangas.\n";
                 }
 
-                printf( "Votre score final est de: $score/3 .\n");
+                echo "Votre score final est de: $score/3 .\n";
                 if($score == 3){
-                    printf( " Score parfait. Vous avez des bonnes bases !\n");
+                    echo " Score parfait. Vous avez des bonnes bases !\n";
                 }
                 else{
-                    printf( "C'est pas encore ça hein.\n");
+                    echo "C'est pas encore ça hein.\n";
                 }
             }
 
