59ae73761060524724917b9e079f6e48203d51fc
Nathan <torres201665@gmail.com>
2023-06-01 13:32:19 +0200

    oui

--- Release2/Sprint1/sites/site1.fr/result_php.php
+++ Release2/Sprint1/sites/site1.fr/result_php.php
@@ -20,16 +20,8 @@
         </ul>
         <div>
         <?php
-            if ((!isset($_GET['calcul']) || !isset($_GET['foot']) || !isset($_GET['atome']) || !isset($_GET['vitesse']) || !isset($_GET['reponse']) || !isset($_GET['answer']) || !isset($_GET['kendo'])) && !isset($_POST['last']))
-            {
-                echo "Il faut répondre à toutes les questions pour soumettre le formulaire.";
 
-                // Arrête l'exécution de PHP
-                return;
-            }
-
-
-            else if (!isset($_POST['last']))
+            if ((isset($_GET['calcul']) || isset($_GET['foot']) || isset($_GET['atome']) || isset($_GET['vitesse']) || isset($_GET['reponse']) || isset($_GET['answer']) || isset($_GET['kendo'])))
             {//Utilisation de GET
                 $score = 0;
                 
@@ -98,13 +90,21 @@
                 }
             }
 
-            else
-            {
-                if(strcmp($_POST['last'],"Manhua") != 0){
-                    echo "Dommage, vous ferez peut-être mieux la prochaine fois.";
+            else {
+                if (isset($_POST['last']))
+                {
+                    if(strcmp($_POST['last'],"Manhua") != 0){
+                        echo "Dommage, vous ferez peut-être mieux la prochaine fois.";
+                    }
+                    else{
+                        echo "Bravo, j'admire vos connaissances";
+                    }
                 }
-                else{
-                    echo "Bravo, j'admire vos connaissances";
+
+                else {
+                    echo "Il faut répondre à toutes les questions pour soumettre le formulaire.";
+
+                    return;
                 }
             }
 
