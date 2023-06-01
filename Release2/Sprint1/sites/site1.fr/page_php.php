c40ddb025825fc64ba028e8f6407d9ea02c5f1bf
josias <josias.1810@gmail.com>
2023-06-01 13:55:11 +0200

    aaa

--- Release2/Sprint1/sites/site1.fr/page_php.php
+++ Release2/Sprint1/sites/site1.fr/page_php.php
@@ -15,10 +15,10 @@
 
     <nav>
         <ul>
-            <h2 class="menu"><a href="index.html">Accueil</a></h2>
-            <h2 class="menu"><a href="page1.html">Page1</a></h2>
-            <h2 class="menu"><a href="page_php.php">Page2</a></h2>
-            <h2 class="menu"><a href="http://www.site2.fr:8080">Site 2</a></h2>
+            <li/><h2 class="menu"><a href="index.html">Accueil</a></h2>
+            <li/><h2 class="menu"><a href="page1.html">Page1</a></h2>
+            <li/><h2 class="menu"><a href="page_php.php">Page2</a></h2>
+            <li/><h2 class="menu"><a href="http://www.site2.fr:8080">Site 2</a></h2>
         </ul>
     </nav>
 </header>
